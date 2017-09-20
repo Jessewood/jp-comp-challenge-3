156aca066e3fc1251a8840b8b627889fae4e2617
Jesse Packwood <packwoodjustin@gmail.com>
2017-09-20 13:14:47 -0600

    naming conventions

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function circleOne()  {
-  $('.circ-1').delay(500).fadeIn(1000, function() {
-  $('.circ-1').addClass('circ-1a')
+  $('.circle-1').delay(500).fadeIn(1000, function() {
+  $('.circle-1').addClass('circle-1a')
   })
 }
 
 function circleTwo()  {
-  $('.circ-2').delay(1000).fadeIn(1000, function() {
-  $('.circ-2').addClass('circ-2a')
+  $('.circle-2').delay(1000).fadeIn(1000, function() {
+  $('.circle-2').addClass('circle-2a')
   })
 }
 
 function circleThree()  {
-  $('.circ-3').delay(1500).fadeIn(1000, function() {
-  $('.circ-3').addClass('circ-3a')
+  $('.circle-3').delay(1500).fadeIn(1000, function() {
+  $('.circle-3').addClass('circle-3a')
   })
 }
 
